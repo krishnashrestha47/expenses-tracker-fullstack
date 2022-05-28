7c63c3c409d1bc94cc0c82b34236062336298794
Krishna Shrestha <krish_sth2005@yahoo.com>
2022-05-28 16:49:39 +1000

    Project has been initialized

---
+++ expenses-tracker-api/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.get("*", (req, res) => {
+  res.status(404).send("<h1>404 Not Found</h1>");
+});
+
+app.listen(PORT, (error) => {
+  error && console.log(error);
+
+  console.log(`Server is running on port ${PORT}`);
+});
