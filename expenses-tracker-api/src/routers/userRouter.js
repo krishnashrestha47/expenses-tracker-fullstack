4c3af4813a94b132be0c1a45a0e23393ce95a6ae
Krishna Shrestha <krish_sth2005@yahoo.com>
2022-06-01 00:02:16 +1000

    user has been created from post method

--- expenses-tracker-api/src/routers/userRouter.js
+++ expenses-tracker-api/src/routers/userRouter.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { insertUser } from "../models/userModel/User.model.js";
 
 const router = express.Router();
 
@@ -9,9 +10,30 @@ router.get("/", (req, res) => {
 });
 
 //register a user
-router.post("/", (req, res) => {
-  console.log(req.body);
-  res.send("register user");
+router.post("/", async (req, res) => {
+  try {
+    console.log(req.body);
+    const result = await insertUser(req.body);
+
+    result?._id
+      ? res.json({
+          status: "success",
+          message: "User registered successfully",
+        })
+      : res.json({
+          status: "error",
+          message: "User registration failed, Please try again later",
+        });
+  } catch (error) {
+    let message = error.message;
+    if (error.message.includes("duplicate key error collection")) {
+      message = "User already exists with this email";
+    }
+    res.json({
+      status: "error",
+      message,
+    });
+  }
 });
 
 //login user
