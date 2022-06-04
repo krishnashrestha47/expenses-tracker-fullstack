b67e2c9ebc17aa889df88da058a874b673124af4
Krishna Shrestha <krish_sth2005@yahoo.com>
2022-06-04 22:39:06 +1000

    frontend application initialized

---
+++ expenses-tracker-frontend/src/App.js
@@ -0,0 +1,15 @@
+import { Button } from "react-bootstrap";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Button variant="warning">
+        Hello <i class="fa-brands fa-react"></i>
+      </Button>
+    </div>
+  );
+}
+
+export default App;
