73dcbb0bbca569d5375bf4a030fb3ff96143efec
Krishna Shrestha <krish_sth2005@yahoo.com>
2022-06-24 21:48:06 +1000

    throw user out to login when no info on session storage

--- expenses-tracker-frontend/src/pages/Dashboard.js
+++ expenses-tracker-frontend/src/pages/Dashboard.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { MainLayout } from "../components/layout/MainLayout";
 
 export const Dashboard = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const user = JSON.parse(sessionStorage.getItem("user"));
+    if (!user?._id) navigate("/");
+  }, []);
+
   return (
     <div>
       <MainLayout>Dashboard</MainLayout>
