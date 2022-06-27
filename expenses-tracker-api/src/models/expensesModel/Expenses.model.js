4897a07e989f7724e3fdea21d0b6ff48037951a2
Krishna Shrestha <krish_sth2005@yahoo.com>
2022-06-27 19:53:10 +1000

    model created for expenses with querries

---
+++ expenses-tracker-api/src/models/expensesModel/Expenses.model.js
@@ -0,0 +1,16 @@
+import ExpensesSchema from "./Expenses.Schema.js";
+
+//expenses should be an object
+export const createExpense = (expense) => {
+  return ExpensesSchema.create(expense);
+};
+
+//filter must be an object that should atleast contain the user Id
+export const getExpenses = (filter) => {
+  return ExpensesSchema.find(filter);
+};
+
+//filter must be an object that should atleast contain the user Id and the expenses Id
+export const deleteExpenses = (filter) => {
+  return ExpensesSchema.findOneAndDelete(filter);
+};
