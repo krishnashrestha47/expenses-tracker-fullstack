ff2c7dc4ec76d1bd021c0df0762355064125414b
Krishna Shrestha <krish_sth2005@yahoo.com>
2022-06-29 20:37:55 +1000

    functions and state created to get and store expenses form data

--- expenses-tracker-frontend/src/components/expenses-form/ExpensesForm.js
+++ expenses-tracker-frontend/src/components/expenses-form/ExpensesForm.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Col, Form, Row } from "react-bootstrap";
 
+const initialState = {
+  name: "",
+  amount: "",
+  date: "",
+};
+
 export const ExpensesForm = () => {
+  const [formDt, setFormDt] = useState(initialState);
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormDt({
+      ...formDt,
+      [name]: value,
+    });
+  };
+  console.log(formDt);
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    console.log(formDt);
+  };
+
   return (
-    <Form>
+    <Form onSubmit={handleOnSubmit}>
       <Row className="g-3 bg-warning rounded pb-3 d-flex justify-content-center">
         <Col md="2">
           <Form.Select type="text" placeholder="Last name">
@@ -13,13 +36,31 @@ export const ExpensesForm = () => {
           </Form.Select>
         </Col>
         <Col md="4">
-          <Form.Control type="text" placeholder="Transaction Name" />
+          <Form.Control
+            type="text"
+            name="name"
+            placeholder="Transaction Name"
+            onChange={handleOnChange}
+            required
+          />
         </Col>
         <Col md="2">
-          <Form.Control type="number" placeholder="Amount" />
+          <Form.Control
+            type="number"
+            name="amount"
+            placeholder="Amount"
+            onChange={handleOnChange}
+            required
+          />
         </Col>
         <Col md="2">
-          <Form.Control type="date" placeholder="Date" />
+          <Form.Control
+            type="date"
+            name="date"
+            placeholder="Date"
+            onChange={handleOnChange}
+            required
+          />
         </Col>
         <Col md="1">
           <Button variant="primary" type="submit">
