9a2afbe924d3f208af4d68329876d9cb52370b7d
Krishna Shrestha <krish_sth2005@yahoo.com>
2022-07-01 23:29:20 +1000

    method created in axios helper to send data in db

--- expenses-tracker-frontend/src/helpers/axiosHelper.js
+++ expenses-tracker-frontend/src/helpers/axiosHelper.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const rootUrl = "http://localhost:8000/api/v1";
 const userApi = rootUrl + "/users";
 const loginApi = rootUrl + "/users/login";
+const expensesAPI = rootUrl + "/expenses";
 
 export const postRegister = (formDt) => {
   try {
@@ -21,6 +22,21 @@ export const postLogin = (formDt) => {
   try {
     return axios.post(loginApi, formDt);
   } catch (error) {
+    return {
+      data: {
+        status: "error",
+        message: error.message,
+      },
+    };
+  }
+};
+
+export const postExpense = async (formDt) => {
+  try {
+    const { data } = await axios.post(expensesAPI, formDt);
+    return data;
+  } catch (error) {
+    console.log(error);
     return {
       data: {
         status: "error",
