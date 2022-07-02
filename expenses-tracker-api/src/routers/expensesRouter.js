a9ae7b20a16461b3c681a368679e0bb7a3156d67
Krishna Shrestha <krish_sth2005@yahoo.com>
2022-07-02 23:16:45 +1000

    get method to fetch stored data from backend

--- expenses-tracker-api/src/routers/expensesRouter.js
+++ expenses-tracker-api/src/routers/expensesRouter.js
@@ -1,12 +1,26 @@
 import express from "express";
-import { createExpense } from "../models/expensesModel/Expenses.Model.js";
+import {
+  createExpense,
+  getExpenses,
+} from "../models/expensesModel/Expenses.Model.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json({
-    message: "Welcome to the expenses API get",
-  });
+router.get("/", async (req, res) => {
+  try {
+    const { authorization } = req.headers;
+    //MOdel- get all expenses of userId - authorization
+    const expenses = await getExpenses({ userId: authorization });
+    res.json({
+      status: "success",
+      expenses,
+    });
+  } catch (error) {
+    res.json({
+      status: "error",
+      message: error.message,
+    });
+  }
 });
 
 router.post("/", async (req, res) => {
